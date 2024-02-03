e13db4374c40a22dbb0e8cc769a7907bad69ea43
Rohit-Ganjoo <143324573+Rohit-Ganjoo@users.noreply.github.com>
2024-02-03 14:47:55 +0530

    Updated Road Accident Queries

--- Road Accident Queries.sql
+++ Road Accident Queries.sql
@@ -1,27 +1,13 @@
-use roadaccidents;
-
-show tables;
-
--- Average number of Casulaties in accidents:
-select speed_limit, avg(Number_of_casualties) 
-from roadrash
-group by 1
-order by 1;
-
--- Check how much each severity contribute to the total accident, also display the how much percent each severity accounts for total accidents.
-SELECT Accident_Severity,
-       `Accidents per Severity`,
-       Concat(Round(( `accidents per severity` / `total accidents` ) * 100, 1)," %") AS `Percent Contribution`
-FROM   (SELECT accident_severity,
-               Count(*) AS `Accidents per Severity`,
-               (SELECT Count(*)
-                FROM   roadrash) AS `Total Accidents`
-        FROM   roadrash
-        GROUP  BY 1) x
-ORDER  BY 2 DESC; 
+-- Spatial Analysis
+-- Top 10 Districts that have high accidents:
+SELECT `Local_Authority_(District)`, count(*)
+FROM roadrash
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 10;
 
 
-
+-- Temporal Analysis:
 -- Display the Month on Month change in the Accident Rate, along with that display the Current Month Accidents and Previous Month Accidents.
 WITH Accident_Monthly_Comparison
 AS (
@@ -43,6 +29,7 @@ SELECT *,
 FROM Accident_Monthly_Comparison;
 
 
+
 -- Number of Casualties in each Quarters of each year, and give the percentage change to the consecutive quarter:
 WITH QuarterCasualties
 AS (
@@ -52,7 +39,7 @@ AS (
 	FROM (
 		SELECT year(str_to_date(`Accident Date`, '%d-%m-%Y')) AS Year
 			,Quarter(str_to_date(`Accident Date`, '%d-%m-%Y')) AS Quarter
-			,sum(Number_of_Casualties) AS `Total Casualties`
+			,SUM(Number_of_Casualties) AS `Total Casualties`
 		FROM roadrash
 		GROUP BY year,2) x
 	     )
@@ -61,7 +48,6 @@ SELECT Quarter
 	,Prev_Quarter
 	,CONCAT (round(((`Total Casualties` - Prev_Quarter) / Prev_Quarter * 100), 2)," %") AS `Percentage Change`
 FROM QuarterCasualties;
-
 
 
 -- Side by side comparison of Total Casualties for different years Quarterwise:
@@ -90,5 +76,82 @@ INNER JOIN
 ORDER BY a.Year, a.Quarter;
 
 
+--Severity Analysis
+-- Check how much each severity contribute to the total accident, also display the how much percent each severity accounts for total accidents.
+
+SELECT Accident_Severity,
+       `Accidents per Severity`,
+       Concat(Round(( `accidents per severity` / `total accidents` ) * 100, 1)," %") AS `Percent Contribution`
+FROM   (SELECT accident_severity,
+               Count(*) AS `Accidents per Severity`,
+               (SELECT Count(*)
+                FROM   roadrash) AS `Total Accidents`
+        FROM   roadrash
+        GROUP  BY 1) x
+ORDER  BY 2 DESC; 
 
 
+-- Display which Road_types contributes to more number of accidents
+SELECT Road_type,
+       `Accidents per roadtype`,
+       Concat(Round((`accidents per roadtype`/ `total accidents` ) * 100, 1)," %") AS `Percent Contribution`
+FROM   (SELECT Road_type,
+               Count(*) AS `Accidents per roadtype`,
+               (SELECT Count(*)
+                FROM   roadrash) AS `Total Accidents`
+        FROM   roadrash
+        GROUP  BY 1) x
+ORDER  BY 2 DESC; 
+
+-- Display which Road surface condition contributes to more number of accidents:
+SELECT Road_Surface_Conditions,
+       `Accidents per Surface condition`,
+       Concat(Round((`accidents per Surface condition`/ `total accidents` ) * 100, 1)," %") AS `Percent Contribution`
+FROM   (SELECT Road_Surface_Conditions,
+               Count(*) AS `Accidents per Surface condition`,
+               (SELECT Count(*)
+                FROM   roadrash) AS `Total Accidents`
+        FROM   roadrash
+        GROUP  BY 1) x
+ORDER  BY 2 DESC; 
+
+
+--Junction Analysis:
+-- using the pivot, make a table with Junction detail as index(Row) and Junction Control as Column and value is the Number of Accidents.
+
+SELECT Junction_Detail,
+SUM(case when Junction_Control = 'Authorised Person' THEN `Number of Accidents` ELSE 0 END) AS 'Authorised Person',
+SUM(case when Junction_Control = 'Auto traffic signal' THEN `Number of Accidents` ELSE 0 END) AS 'Auto Traffic Signal',
+SUM(case when Junction_Control = 'Data missing or out of range' THEN `Number of Accidents` ELSE 0 END) AS 'Data Missing',
+SUM(case when Junction_Control = 'Give way or uncontrolled' THEN `Number of Accidents` ELSE 0 END) AS 'Give way or uncontrolled',
+SUM(case when Junction_Control = 'Not at junction or within 20 metres' THEN `Number of Accidents` ELSE 0 END) AS 'Not at junction nearby',
+SUM(case when Junction_Control = 'Stop sign' THEN `Number of Accidents` ELSE 0 END) AS 'Stop Sign'
+FROM
+(
+SELECT  Junction_Control, Junction_Detail,count(*) as `Number of Accidents` 
+FROM roadrash
+GROUP BY 1,2
+ORDER BY 1
+)x
+group by 1;
+
+-- Road Surface Conditions VS Vehicle Types :
+-- Display the Pivot Table, that shows the index 
+SELECT Vehicle_Type,
+SUM(case when Road_Surface_Conditions = 'Dry' THEN `Total Casualties` ELSE 0 END) AS 'Dry',
+SUM(case when Road_Surface_Conditions = 'Wet' THEN `Total Casualties` ELSE 0 END) AS 'Wet',
+SUM(case when Road_Surface_Conditions = 'Frosty' THEN `Total Casualties` ELSE 0 END) AS 'Frosty',
+SUM(case when Road_Surface_Conditions = 'Snowy' THEN `Total Casualties` ELSE 0 END) AS 'Snowy',
+SUM(case when Road_Surface_Conditions = 'Damp' THEN `Total Casualties` ELSE 0 END) AS 'Damp',
+SUM(case when Road_Surface_Conditions = 'Other' THEN `Total Casualties` ELSE 0 END) AS 'Other'
+FROM
+(
+select distinct Road_Surface_Conditions,Vehicle_Type,sum(Number_of_Casualties) as `Total Casualties` 
+from roadrash
+group by 1,2
+order by 1
+)x
+GROUP BY 1;
+
+
+select distinct Road_Surface_Conditions from roadrash;
